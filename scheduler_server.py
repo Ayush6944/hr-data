e163d5062352ceba00e9a081a5eb89ed96976923
Ayush Srivastava <114403598+Ayush6944@users.noreply.github.com>
2025-07-31 13:52:54 +0530

    nnn

--- scheduler_server.py
+++ scheduler_server.py
@@ -9,8 +9,8 @@ import requests
 from typing import Optional
 
 # ===================== CONFIGURATION =====================
-SCHEDULE_HOUR = 1 # 9 PM (used after first run)
-SCHEDULE_MINUTE = 15 # 46 minutes (used after first run)
+SCHEDULE_HOUR = 14 # 9 PM (used after first run)
+SCHEDULE_MINUTE = 00 # 46 minutes (used after first run)
 IST = pytz.timezone('Asia/Kolkata')
 LOG_FILE = 'scheduler_audit.log'
 LOGIN_LOG_FILE = 'login_audit.log'
